4f890f6398875be36f3ecf9f2e0c06fe98eec59a
yuki-1234 <56993732+yuki-1234@users.noreply.github.com>
2020-04-05 17:37:09 +0200

    Add files via upload

---
+++ encryption.py
@@ -0,0 +1,26 @@
+# Encode plaintext
+plain = input("input Plaintext : ")
+key = input("input key : ")
+plain = plain.upper()
+plain = plain.replace(' ', '')
+key = key.upper()
+alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+plain_shift = []
+key_shift = []
+cipher = []
+
+for i in range(len(plain)):
+    for j in range(len(alphabet)):
+        if plain[i] == alphabet[j]:
+            plain_shift.append(j)
+
+for i in range(len(key)):
+    for j in range(len(alphabet)):
+        if key[i] == alphabet[j]:
+            key_shift.append(j)
+
+for i in range(len(plain)):
+    if i == 0:
+        print("ciphertext is : ", end="")
+    cipher.append((plain_shift[i] + key_shift[i % len(key)]) % 26)
+    print(alphabet[(plain_shift[i] + key_shift[i % len(key)]) % 26], end="")
